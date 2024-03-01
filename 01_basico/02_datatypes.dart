b10a32d16f80274996c46b20c047473c09cd013b
sennin7k <sennin7k@hotmail.com>
2024-03-01 16:59:17 -0500

    tipo de datos numericos

---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,17 @@
+main() {
+  // ===== Números
+
+  // al colocar var la varible va a tener el valor que le asigno
+  var a = 10;
+
+  // entero
+  int b = 10;
+
+  // decimal
+  double c = 5.5;
+
+  // el signo ? le dice que d puede ser nulo
+  int? d;
+
+  print(d);
+}
