914b7869817569b3d595fa34b8ed68e94fcbbdf7
sennin7k <sennin7k@hotmail.com>
2024-03-01 21:52:00 -0500

    Comentarios

---
+++ 01_basico/04_comentarios.dart
@@ -0,0 +1,19 @@
+/**
+ * Este archivo sirve para explicar los comentarios
+ * [creadoEn]: hoy
+ * 
+ */
+void main(List<String> args) {
+  // Esta es una variable
+  int a = 10; //a debería de ser 10
+/*
+  final personas = [
+    'Pedro',
+    'Juan',
+  ];
+  */
+}
+
+/// Funciona para saludar
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar(String nombre, String mensaje) {}
